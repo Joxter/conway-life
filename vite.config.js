985e56c75360504db24006f3a5b280498349e454
Joxter <joxterrr@gmail.com>
2022-09-16 21:42:35 +0200

    update build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { babel } from '@rollup/plugin-babel';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [babel({ extensions: ['.ts'], babelHelpers: 'bundled' })],
+  build: {
+    target: 'esnext', // --target=es2020
+  },
+  base: './',
 });
